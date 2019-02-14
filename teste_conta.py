129dc8bc7e2ce970ed4d8cf0d5cf5e671a09e3be
Diego Silva <38412142+di3goCS@users.noreply.github.com>
2019-02-13 22:04:23 -0200

    Update teste_conta.py

--- teste_conta.py
+++ teste_conta.py
@@ -1,25 +1,33 @@
 import unittest
+import conta
+'''
+- Alisson Santana
+- Davy Matos
+- Diego Silva
+- Laercio Rios
+- Luciano Teles
+'''
 
 
 class TesteConta(unittest.TestCase):
 
     def testCadastrar(self):
-        self.assertTrue(Conta.cadastararContra(['99999999999', 'José da Silva']))
-        self.assertFalse(Conta.cadastararContra(['999999', ' ']))
+        self.assertTrue(conta.cadastararContra(['99999999999', 'José da Silva']))
+        self.assertFalse(conta.cadastararContra(['999999', ' ']))
 
     def testExculir(self):
-        self.assertTrue(Conta.excluirContra(['99999999999', 0.00]))
-        self.assertFalse(Conta.excluirConta(' '))
-        self.assertFalse(Conta.excluirConta('123'))
-        self.assertFalse(Conta.excluirConta(['99999999999', 10.00]))
+        self.assertTrue(conta.excluirContra(['99999999999', 0.00]))
+        self.assertFalse(conta.excluirConta(' '))
+        self.assertFalse(conta.excluirConta('123'))
+        self.assertFalse(conta.excluirConta(['99999999999', 10.00]))
 
     def testDepositar(self):
-        self.assertTrue(Conta.depositar([5.00]))
-        self.assertFalse(Conta.depositar(['0.00']))
+        self.assertTrue(conta.depositar([5.00]))
+        self.assertFalse(conta.depositar(['0.00']))
 
     def testSaldo(self):
-        self.assertTrue(Conta.alterarLimite(2.00))
-        self.assertFalse(Conta.alterarLimite(''))
+        self.assertTrue(conta.alterarLimite(2.00))
+        self.assertFalse(conta.alterarLimite(''))
 
     def test_transferencia(self):
         self.assertTrue(conta.transferencia((conta.buscarConta("145178-9")), 150))
